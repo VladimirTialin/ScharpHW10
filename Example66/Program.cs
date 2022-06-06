c886af92206bedfd318c0cd2ca366d6033cf5e17
VladimirTialin <103684564+VladimirTialin@users.noreply.github.com>
2022-06-06 22:32:26 +0300

    Решил задачу 66

---
+++ Example66/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+int Number(int numberOne, int numberTwo)
+{
+    if (numberOne==numberTwo)
+    {
+        return numberOne;
+    }
+    else if (numberOne>numberTwo)
+    {
+        return numberTwo+Number(numberOne,numberTwo+1);
+    }
+    else
+    {
+         return numberOne+Number(numberOne+1,numberTwo);
+
+    }
+}
+Console.WriteLine("Введите 2 числа: ");
+int numberOne=Convert.ToInt32(Console.ReadLine());
+int numberTwo=Convert.ToInt32(Console.ReadLine());
+int result = Number(numberOne, numberTwo);
+Console.WriteLine($"M = {numberOne}; N = {numberTwo} - > {result}");
